58f02f5b459a613f96ad0b7ebda092e05faa6733
Christian Glazier <christian.glazier@gmail.com>
2019-02-14 16:32:30 -0600

    Part 2 complete plus nav

--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -1,6 +1,15 @@
 package org.launchcode.models;
 
+import org.springframework.data.annotation.Id;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 
@@ -14,13 +23,17 @@ public class Category {
     @Size(min=3, max=15)
     private String name;
 
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<Cheese> cheeses = new ArrayList<>();
+
     Category(){}
 
-    Category(name) {
+    Category(String name) {
         this.name = name;
     }
 
-    public void setName( String) {
+    public void setName(String name) {
         this.name = name;
     }
     public String getName() {
